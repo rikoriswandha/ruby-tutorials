9692693b2381268aa46ba0616a4f0998a5b0b7ac
Riko Riswandha <rikorisw@gmail.com>
2018-05-12 16:59:24 +0800

    Update sedikit syntax keliru

--- exercise3.rb
+++ exercise3.rb
@@ -3,7 +3,7 @@ puts "Masukkan angka kesukaan anda!"
 angka = gets.to_i
 #Validasi pertama adalah 3 & 5, karena jika diletakkan di belakang akan dilewatkan begitu dibagi angka 3 atau 5 tercapai
 #Menggunakan operator %(modulo), sisa bagi == 0
-if angka % 3 && angka % 5 == 0 
+if angka % 3 == 0 && angka % 5 == 0 
   puts "FizzBuzz"
 elsif angka % 3 == 0
   puts "Fizz"
